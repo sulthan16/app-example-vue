458d97bde647b02ffcb432ca5ba4ae9033fa9581
sulthan <sulthana672@gmail.com>
2018-12-05 15:22:49 +0700

    config.example.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from 'axios'
+import './config'
 import './registerServiceWorker'
 import './mixin'
 
 Vue.config.productionTip = false
-
-axios.defaults.baseURL = 'http://localhost:3000'
-axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
 
 router.beforeEach((to, from, next) => {
     if(to.name == 'login'){
